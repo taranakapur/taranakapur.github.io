7356c4397db4983b76badaa44b1621aa07fedec4
VinayakDubey26 <vinayakdube268@gmail.com>
2025-02-14 02:33:07 +0530

    Update vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/taranakapur.github.io/', // Change this to match your repo name
+  base: 'https://taranakapur.github.io/', 
 });
